4c43c8626d586995ba7ab59e0794cc50f5352275
Normal_Pete <petrohorokh@gmail.com>
2024-04-29 21:40:49 +0300

    Add project files.

---
+++ IntegrationApproximation.cs
@@ -0,0 +1,66 @@
+﻿namespace Lab7;
+
+public static class IntegrationApproximation
+{
+    public static int LeftRectangleFunctionCall { get; set; }
+    public static int MiddleRectangleFunctionCall { get; set; }
+    public static double Function(double x) { return 1 / (x * Math.Pow(Math.Log(x), 2)); }
+    public static double Trapeze(int n, double a, double b, double h)
+    {
+        double sum = 0;
+        double x = a;
+        double f0 = Function(a);
+        double fn = Function(b);
+        for (int i = 1; i < n; i++)
+        {
+            x += h;
+            sum += Function(x);
+        }
+        return h * (0.5 * (f0 + fn) + sum);
+    }
+
+    public static double LeftRectangle(int n, double a, double b, double h, double prevI)
+    {
+        var sum = 0.0;
+        LeftRectangleFunctionCall = 0;
+        for (int i = 1; i < n; i += 2)
+        {
+            var x = a + h * i;
+            sum += Function(x);
+            LeftRectangleFunctionCall++;
+        }
+        return prevI / 2 + h * sum;
+    }
+    public static double MiddleRectangle(int n, double a, double b, double h)
+    {
+        MiddleRectangleFunctionCall = 0;
+        double x = a + .5 * h;
+        double sum = Function(x);
+        MiddleRectangleFunctionCall++;
+        for (int i = 2; i < n + 1; i++)
+        {
+            x += h;
+            sum += Function(x);
+            MiddleRectangleFunctionCall++;
+        }
+        return h * sum;
+    }
+    public static double Simpson(int n, double a, double b, double h)
+    {
+        var x = a;
+        var f = new List<double> { Function(a) };
+        var sum1 = 0.0;
+        var sum2 = 0.0;
+        for (int i = 1; i < n + 1; i++)
+        {
+            x += h;
+            f.Add(Function(x));
+        }
+        for (int i = 1; i < n; i += 2) { sum1 += f[i]; }
+        for (int i = 2; i < n; i += 2) { sum2 += f[i]; }
+        var f0 = f.First();
+        var fn = f.Last();
+        return h / 3 * (f0 + 4 * sum1 + 2 * sum2 + fn);
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using Lab7;
+
+var a = 2.0;
+var b = 3.0;
+var e = 1E-5;
+var preciseIntegral = .532456;
+var rM = 0.0;
+var rL = 0.0;
+var rT = 0.0;
+var rS = 0.0;
+var hM = 0.0;
+var hL = 0.0;
+var hT = 0.0;
+var hS = 0.0;
+var iM = preciseIntegral;
+var iL = IntegrationApproximation.LeftRectangle(4, a, b, (b - a) / 2, 0.0);
+var iT = preciseIntegral;
+var iS = preciseIntegral;
+Console.WriteLine($"Precise integral - {preciseIntegral}");
+for (int n = 4; ; n *= 2){ 
+    var h = (b - a) / n;
+    if (hM == 0)
+    {
+        iM = IntegrationApproximation.MiddleRectangle(n, a, b, h);
+        if (Math.Abs(preciseIntegral - iM) < e)
+        {
+            hM = h;
+            rM = iM;
+        }
+    }
+    if (hL == 0)
+    {
+        iL = IntegrationApproximation.LeftRectangle(n, a, b, h, iL);
+        if (Math.Abs(preciseIntegral - iL) < e)
+        {
+            hL = h;
+            rL = iL;
+        }
+    }
+    if (hT == 0)
+    {
+        iT = IntegrationApproximation.Trapeze(n, a, b, h);
+        if (Math.Abs(preciseIntegral - iT) < e)
+        {
+            hT = h;
+            rT = iT;
+        }
+    }
+    if (hS == 0)
+    {
+        iS = IntegrationApproximation.Simpson(n, a, b, h);
+        if (Math.Abs(preciseIntegral - iS) < e)
+        {
+            hS = h;
+            rS = iS;
+        }
+    }
+    if (Math.Abs(preciseIntegral - iM) < e && Math.Abs(preciseIntegral - iL) < e && Math.Abs(preciseIntegral - iT) < e && Math.Abs(preciseIntegral - iS) < e) break;
+}
+Console.WriteLine("Results after all approximations met precision level:");
+Console.WriteLine($"Integration with formula of middle rectangles result - {rM}");
+Console.WriteLine($"Step - {hM}. Amount of steps = {(b - a) / hM}");
+Console.WriteLine($"Integration with formula of left rectangles result - {rL}");
+Console.WriteLine($"Step - {hL}. Amount of steps = {(b - a) / hL}");
+Console.WriteLine($"Integration with trapezium formula - {rT}");
+Console.WriteLine($"Step - {hT}. Amount of steps = {(b - a) / hT}");
+Console.WriteLine($"Integration with Simpson's formula - {rS}");
+Console.WriteLine($"Step - {hS}. Amount of steps = {(b - a) / hS}");
+Console.WriteLine($"Left function use - {IntegrationApproximation.LeftRectangleFunctionCall}");
+Console.WriteLine($"Middle function use - {IntegrationApproximation.MiddleRectangleFunctionCall}");
+Console.WriteLine($"Incoherency with with formula of middle rectangles - {Math.Abs(preciseIntegral - rM)}");
+Console.WriteLine($"Incoherency with formula of left rectangles - {Math.Abs(preciseIntegral - rL)}");
+Console.WriteLine($"Incoherency with trapezium formula - {Math.Abs(preciseIntegral - rT)}");
+Console.WriteLine($"Incoherency with Simpson's formula - {Math.Abs(preciseIntegral - rS)}");
